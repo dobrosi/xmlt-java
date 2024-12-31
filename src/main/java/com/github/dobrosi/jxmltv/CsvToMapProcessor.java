fb4d68b92742235573dfd7ed5920f56e6da8d706
dobrosi.andras <dobrosi.andras@microsec.hu>
2024-12-31 16:00:10 +0100

    use imgUrl for mapping

--- src/main/java/com/github/dobrosi/jxmltv/CsvToMapProcessor.java
+++ src/main/java/com/github/dobrosi/jxmltv/CsvToMapProcessor.java
@@ -7,13 +7,31 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
+import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
+import lombok.val;
 
 @Slf4j
 public class CsvToMapProcessor {
-    public static Map<String, Map<String, String>> getMap() {
+    @Data
+    public static class Channel {
+        private String originalId;
+        private String id;
+        private String name;
+        private String imgUrl;
+
+        public Channel(final String[] parts) {
+            originalId = parts[0].trim();
+            id = parts[1].trim();
+            String word;
+            name = parts.length > 2 && !(word = parts[2].trim()).isBlank() ? word : id;
+            imgUrl = parts.length > 3 && !(word = parts[3].trim()).isBlank() ? word : null;
+        }
+    }
+
+    public static Map<String, Map<String, Channel>> getMap() {
         String directoryPath = "mapper";
-        Map<String, Map<String, String>> result = processCsvFiles(directoryPath);
+        val result = processCsvFiles(directoryPath);
         result.forEach((fileName, fileContentMap) -> {
             System.out.println("File: " + fileName);
             fileContentMap.forEach((key, value) ->
@@ -22,8 +40,8 @@ public class CsvToMapProcessor {
         return result;
     }
 
-    private static Map<String, Map<String, String>> processCsvFiles(String directoryPath) {
-        Map<String, Map<String, String>> csvDataMap = new HashMap<>();
+    private static Map<String, Map<String, Channel>> processCsvFiles(String directoryPath) {
+        Map<String, Map<String, Channel>> csvDataMap = new HashMap<>();
         File directory = new File(directoryPath);
 
         if (!directory.exists() || !directory.isDirectory()) {
@@ -37,7 +55,7 @@ public class CsvToMapProcessor {
             for (File file : files) {
                 String fileNameWithoutExtension = file.getName()
                     .replaceFirst("\\.[^.]+$", "");
-                Map<String, String> fileContentMap = processCsvFile(file);
+                Map<String, Channel> fileContentMap = processCsvFile(file);
                 csvDataMap.put(fileNameWithoutExtension, fileContentMap);
             }
         }
@@ -45,15 +63,15 @@ public class CsvToMapProcessor {
         return csvDataMap;
     }
 
-    private static Map<String, String> processCsvFile(File file) {
-        Map<String, String> contentMap = new HashMap<>();
+    private static Map<String, Channel> processCsvFile(File file) {
+        Map<String, Channel> contentMap = new HashMap<>();
 
         try (BufferedReader br = new BufferedReader(new FileReader(file))) {
             String line;
             while ((line = br.readLine()) != null) {
                 String[] parts = line.split(",", -1);
                 if (parts.length >= 2 && !parts[0].isBlank()) {
-                    contentMap.put(parts[0].trim(), parts[1].trim());
+                    contentMap.put(parts[0].trim(), new Channel(parts));
                 }
             }
         } catch (IOException e) {
--- src/main/java/com/github/dobrosi/jxmltv/JxmltvApplication.java
+++ src/main/java/com/github/dobrosi/jxmltv/JxmltvApplication.java
@@ -6,6 +6,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Function;
 
 import lombok.extern.slf4j.Slf4j;
 import lombok.val;
@@ -15,32 +16,41 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 @Slf4j
 public class JxmltvApplication {
-
+	public static final String CHANNEL_PREFIX = "<channel id=\"";
+	public static final String DISPLAY_NAME_PREFIX = "<display-name>";
+	public static final String ICON_PREFIX = "<icon src=\"";
+	public static final String DISPLAY_NAME_POSTFIX = "</display-name>";
+	public static final String CHANNEL_POSTFIX = "</channel>";
+	public static final String PROGRAMME_PREFIX = "<programme";
+	public static final String DIGI = "digi";
+	private String theKey;
+	private boolean finishedChannelParsing;
 	private String pChannelId;
-
-	private String pLine;
-
+	private String pImgUrl;
+	private String pChannelLine;
 	private Map<String, String> ids;
-
-	private Map<String, String> channelNameMap;
+	private Map<String, CsvToMapProcessor.Channel> channelMap;
 
 	public static void main(String[] args) {
 		SpringApplication.run(JxmltvApplication.class, args);
 	}
 
 	public JxmltvApplication() {
-		start("digi");
+		theKey = DIGI;
+		start();
 		val map = CsvToMapProcessor.getMap();
 		for (val key : map.keySet()) {
-			channelNameMap = map.get(key);
-			start(key);
+			theKey = key;
+			channelMap = map.get(key);
+			start();
 		}
 	}
 
-	private void start(String key) {
+	private void start() {
 		var filePath = "channels.xml";
-		var outputFilePath = "channels-" + key + ".xml";
+		var outputFilePath = "channels-" + theKey + ".xml";
 		ids = new HashMap<>();
+		finishedChannelParsing = false;
 
 		try {
 			try(var writer = Files.newBufferedWriter(Path.of(outputFilePath))) {
@@ -59,23 +69,66 @@ public class JxmltvApplication {
 	}
 
 	private void parse(BufferedWriter writer, String line) throws IOException {
-		if (pLine != null && line.contains("<display-name>")) {
-			String channelName = getWord(line, "<display-name>", "</display-name>");
-			ids.put(pChannelId, mapName(channelName));
-			write(writer, pLine);
-			line = line.replace(channelName, mapName(channelName));
-			pLine =	null;
-			pChannelId = null;
-		} else if (line.startsWith("<channel id=\"")) {
-			pChannelId = getWord(line, "<channel id=\"", "\"");
-			pLine = line;
-			return;
+		if (!finishedChannelParsing) {
+			if (line.contains(CHANNEL_PREFIX)) {
+				pChannelId = getChannelId(line);
+				pChannelLine = line;
+				return;
+			} else if (line.contains(DISPLAY_NAME_PREFIX)) {
+				if (pChannelLine != null) {
+					String displayName = getWord(line, DISPLAY_NAME_PREFIX, DISPLAY_NAME_POSTFIX);
+					ids.put(pChannelId, mapId(displayName));
+					write(writer, pChannelLine);
+					line = line.replace(displayName, mapName(displayName));
+					pChannelLine = null;
+					pImgUrl = mapImgUrl(displayName);
+				} else if (isNotDigi()) {
+					return;
+				}
+			} else if (line.contains(ICON_PREFIX)) {
+				if (isNotDigi()) {
+					return;
+				}
+			} else if (line.contains(CHANNEL_POSTFIX)) {
+				if (pImgUrl != null) {
+					write(writer, "  <icon src=\"" + pImgUrl + "\"/>");
+				}
+				pChannelId = null;
+				pImgUrl = null;
+			} else if (line.contains(PROGRAMME_PREFIX)) {
+				finishedChannelParsing = true;
+			}
 		}
 		write(writer, line);
 	}
 
-	private String mapName(String channelName) {
-		return channelNameMap == null ? channelName : channelNameMap.getOrDefault(channelName, channelName);
+	private boolean isNotDigi() {
+		return !theKey.equals(DIGI);
+	}
+
+	private String mapValue(String displayName, Function<CsvToMapProcessor.Channel, String> getter) {
+		return mapValue(displayName, displayName, getter);
+	}
+
+	private String mapValue(String displayName, String defaultValue, Function<CsvToMapProcessor.Channel, String> getter) {
+		CsvToMapProcessor.Channel channel;
+		return channelMap == null || (channel = getChannel(displayName)) == null ? defaultValue : getter.apply(channel);
+	}
+
+	private String mapImgUrl(String displayName) {
+		return mapValue(displayName, null, CsvToMapProcessor.Channel::getImgUrl);
+	}
+
+	private String mapId(String displayName) {
+		return mapValue(displayName, CsvToMapProcessor.Channel::getId);
+	}
+
+	private String mapName(String displayName) {
+		return mapValue(displayName, CsvToMapProcessor.Channel::getName);
+	}
+
+	private CsvToMapProcessor.Channel getChannel(final String displayName) {
+		return channelMap.get(displayName);
 	}
 
 	private void write(BufferedWriter writer, String line) throws IOException {
@@ -85,6 +138,10 @@ public class JxmltvApplication {
 		}
 		writer.write(replacedLine);
 		writer.newLine();
+	}
+
+	private String getChannelId(String line) {
+		return getWord(line, CHANNEL_PREFIX, "\"");
 	}
 
 	private String getWord(String line, String d1, String d2) {
